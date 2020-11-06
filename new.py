8fe4f2c90600d1c236ce8209273cb2bae193270d
Lucas <lukas.zahoransky@zsbystricany.info>
2020-11-06 17:41:27 +0100

    Made easier to automate

--- new.py
+++ new.py
@@ -1,32 +1,32 @@
 import os
 import sys
 import requests
-import argparse
-from secrets import token
+from secrets import token, username
 
 path = 'C:/Coding/my-repositories'
 
 
-def arguments():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--name', '-n', type=str, dest='name', required=True)
-	parser.add_argument('--private', '-p', dest='private', action='store_true')
-	arguments = parser.parse_args()
-	return arguments
+def prompt():
+	project_name = input("Enter your project's name: ")
+	private_input = input('Do you want your repository to be private? (Y/N): ')
+	private = False
+
+	if private_input == 'N':
+			private = True
+
+	create_dir(project_name)
+	create_repo(project_name, private)
+	setup(project_name)
 
 
-def create_dir():
-	project_name = arguments().name
+def create_dir(project_name):
+	project_name = 
 	os.system(f'cd {path} && mkdir {project_name}')
 
 
-def create_repo():
-	project_name = arguments().name
-	is_private = arguments().private
-	print(is_private)
-	
+def create_repo(project_name, private):
 	url = 'https://api.github.com'
-	if is_private:
+	if private:
 		data = '{"name": "' + project_name + '", "private": true}'
 	else:
 		data = '{"name": "' + project_name + '", "private": false}'
@@ -37,10 +37,9 @@ def create_repo():
 	response = requests.request('POST', f'{url}/user/repos', data = data, headers = headers)
 
 
-def setup():
-	project_name = arguments().name
+def setup(project_name):
 	get_path = f'cd {path}/{project_name}'
-	url = f'https://github.com/scaldings/{project_name}.git'
+	url = f'https://github.com/{username}/{project_name}.git'
 	os.system(f'{get_path} && echo # {project_name} > README.md')
 	os.system(f'{get_path} && git init')
 	os.system(f'{get_path} && git add .')
@@ -48,9 +47,8 @@ def setup():
 	os.system(f'{get_path} && git init')
 	os.system(f'{get_path} && git remote add origin {url}')
 	os.system(f'{get_path} && git push -u origin master')
+	os.system(f'{get_path} && code .')
 
 
 if __name__ == '__main__':
-	create_dir()
-	create_repo()
-	setup()
+	prompt()
