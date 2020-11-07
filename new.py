e25606b70f14015cbd9177782970de9b7a65e2d9
Luke <lukas.zahoransky@zsbystricany.info>
2020-11-07 17:27:29 +0100

    Add the creation of a virtual environment

--- new.py
+++ new.py
@@ -9,9 +9,11 @@ path = 'C:/Coding/my-repositories'
 def prompt():
 	project_name = input("Enter your project's name: ")
 	private_input = input('Do you want your repository to be private? (Y/N): ')
-	private = False
+	private = True
 
 	if private_input == 'N':
+			private = False
+	elif private_input == 'Y':
 			private = True
 
 	create_dir(project_name)
@@ -20,7 +22,6 @@ def prompt():
 
 
 def create_dir(project_name):
-	project_name = 
 	os.system(f'cd {path} && mkdir {project_name}')
 
 
@@ -41,13 +42,17 @@ def setup(project_name):
 	get_path = f'cd {path}/{project_name}'
 	url = f'https://github.com/{username}/{project_name}.git'
 	os.system(f'{get_path} && echo # {project_name} > README.md')
+	os.system(f'{get_path} && echo # Hello world! > {project_name}.py')
 	os.system(f'{get_path} && git init')
 	os.system(f'{get_path} && git add .')
 	os.system(f'{get_path} && git commit -m "Initial commit"')
 	os.system(f'{get_path} && git init')
 	os.system(f'{get_path} && git remote add origin {url}')
 	os.system(f'{get_path} && git push -u origin master')
+	os.system(f'{get_path} && virtualenv venv')
+	os.system(f'{get_path}/venv/Scripts && activate.bat')
 	os.system(f'{get_path} && code .')
+	os.system('exit')
 
 
 if __name__ == '__main__':
